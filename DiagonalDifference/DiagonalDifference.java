1cfb3d5b3afb6c2a06bc572c3723075c900e1b32
NigelTanYong <nigel.ty28@gmail.com>
2025-07-01 21:56:22 +0800

    Create DiagonalDifference.java

---
+++ DiagonalDifference/DiagonalDifference.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+    //Time Complexity: O(n), Space Complexity: O(1)
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+        int firstDiagonal = 0, secondDiagonal = 0;
+        int arrSize = arr.size(); //size of rows
+        for(int i=0; i<arrSize; i++){
+            firstDiagonal += arr.get(i).get(i);
+            secondDiagonal += arr.get(i).get(arrSize-1-i);
+        }
+        return Math.abs(firstDiagonal - secondDiagonal);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
