290d5829ef8e38dee64a6c30a9e8ed4fbbddbcde
NigelTanYong <nigel.ty28@gmail.com>
2025-07-11 22:27:02 +0800

    Create RecursiveDigitSum.js

---
+++ RecursiveDigitSum/RecursiveDigitSum.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
+function superDigit(n, k) {
+    // Write your code here
+    //sum the digits
+    let sum = 0;
+    for(let i=0; i<n.length; i++){
+        sum+=Number(n[i]);
+    }
+    //mutiply k
+    sum *= k;
+    
+    //sum till single digit
+    while(sum>=10){
+        let temp = 0;
+        while(sum>0){
+            temp += sum%10;
+            sum=Math.floor(sum/10);
+        }
+        sum = temp;
+    }
+    return sum;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = firstMultipleInput[0];
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const result = superDigit(n, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
