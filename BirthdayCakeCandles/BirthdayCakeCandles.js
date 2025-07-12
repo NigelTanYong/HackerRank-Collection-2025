94e9ef2370fe04f0166bccba551b7f7905e6e224
NigelTanYong <nigel.ty28@gmail.com>
2025-07-12 21:06:53 +0800

    Create BirthdayCakeCandles.js

---
+++ BirthdayCakeCandles/BirthdayCakeCandles.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    candles.sort((a, b) => a - b); // numeric sort
+    //get tallest height
+    let count=0, i=candles.length -1, tallest=candles[i], height=tallest;
+    while (height === tallest && i>=0){
+        count++;
+        i--;
+        height = candles[i];
+        
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
